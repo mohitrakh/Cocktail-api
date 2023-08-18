e422e1b0684456543f524673816bfdfd121baba8
Mohit <89239016+mohitrakh@users.noreply.github.com>
2023-08-18 16:04:16 +0530

    added files
    files uploaded

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# Cocktail-api
-cocktail details site which uses cocktail api to get data 
+#### React Router Fix
+
+(Fix)[https://dev.to/dance2die/page-not-found-on-netlify-with-react-router-58mc]
+
+#### CRA Fix
+
+```
+
+"build": "CI= react-scripts build",
+
+```
+
+```sh
+npm install react-router-dom@6
+```
---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. At blanditiis,
+        saepe dolorum facere sequi esse accusamus quidem accusantium maiores
+        exercitationem, non suscipit, perferendis ullam. Debitis id ratione
+        quibusdam eligendi, totam deleniti corporis officia facilis tempora rem
+        corrupti minima aspernatur aliquam.
+      </p>
+    </section>
+  );
+};
+
+export default About;
